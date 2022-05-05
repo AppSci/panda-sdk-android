674f1238f4c38e5fd0ae6756aae4ec60c4f44131
Dmytro Ostapovets <dmytro.ostapovets@gen.tech>
2022-05-05 17:28:36 +0300

    logs added

--- sdk/src/main/java/com/appsci/panda/sdk/data/subscriptions/SubscriptionsRepositoryImpl.kt
+++ sdk/src/main/java/com/appsci/panda/sdk/data/subscriptions/SubscriptionsRepositoryImpl.kt
@@ -96,6 +96,7 @@ class SubscriptionsRepositoryImpl(
     override fun getSubscriptionScreen(type: ScreenType?, id: String?): Single<SubscriptionScreen> {
         val key = ScreenKey(id = id, type = type)
         val cachedScreen = loadedScreens[key]
+        Timber.d("getSubscriptionScreen")
         return (if (cachedScreen != null) {
             Single.just(cachedScreen)
         } else {
@@ -106,6 +107,8 @@ class SubscriptionsRepositoryImpl(
                     name = it.name,
                     screenHtml = it.screenHtml
             )
+        }.doOnSuccess {
+            Timber.d("getSubscriptionScreen finished")
         }
 
     }
@@ -184,6 +187,7 @@ class SubscriptionsRepositoryImpl(
                             type = type?.requestName,
                             id = id)
                 }.doOnSuccess {
+                    Timber.d("loadSubscriptionScreen finished $type\n$id")
                     loadedScreens[key] = it
                 }
     }
